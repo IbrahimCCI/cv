7acb2b11dbed8c67e03fbe2115b2671aa218c80a
IbrahimCCI <46899376+IbrahimCCI@users.noreply.github.com>
2019-01-30 11:51:51 +0100

    Update script-cv.js

--- script-cv.js
+++ script-cv.js
@@ -3,5 +3,14 @@ window.addEventListener("scroll", function() { //ajoute une fonction efectuée l
 
 // Calculez le déplacement ici ...
 console.log(window.scrollY);
+
 document.getElementById('progress_bar').value = window.scrollY; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 });
+
+function myScroll() { // Définition de la fonction de scroll
+    var progress = document.getElementById('progress_bar').value; // Valeur de la barre de progression
+    
+    // Calculez ici la valeur de déplacement à efectuer lors du scroll
+    
+    window.scroll(0, value) // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
+}
