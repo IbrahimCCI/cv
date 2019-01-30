4e1cdea88130bebb2b82af97a106e03fefd25f91
IbrahimCCI <46899376+IbrahimCCI@users.noreply.github.com>
2019-01-30 09:59:33 +0100

    Add files via upload

---
+++ script-cv.js
@@ -0,0 +1,8 @@
+
+<p style="width:300px">
+  <input type="range" id="s1" min="0" max="10" value="4" step="2">
+</p>
+
+<p style="width:300px">
+  <input class="mdl-slider mdl-js-slider" type="range" id="s1" min="0" max="10" value="4" step="2">
+</p>
