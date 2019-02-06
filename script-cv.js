cbe6240c3cc55576950981bb3fa5446ecdc63a3a
IbrahimCCI <46899376+IbrahimCCI@users.noreply.github.com>
2019-02-06 20:09:54 +0100

    Update script-cv.js

--- script-cv.js
+++ script-cv.js
@@ -1,10 +1,11 @@
 
-/*window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
+window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
 
 // Calculez le déplacement ici ...
 console.log(window.scrollY);
 
-document.getElementById('progress_bar').value = window.scrollY; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
+document.getElementById('progress_bar').value = (window.scrollY*100)/(document.body.offsetHeight-window.innerHeight);
+// Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 });
 
 function myScroll() { // Définition de la fonction de scroll
@@ -13,12 +14,12 @@ function myScroll() { // Définition de la fonction de scroll
     // Calculez ici la valeur de déplacement à efectuer lors du scroll
     
     window.scroll(0, value) // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
-}*/
+}
 
-window.addEventListener("scroll", function() {
+/*window.addEventListener("scroll", function() {
 let deplacement  = (window.scrollY*100)/(document.body.offsetHeight-window.innerHeight);
 
     if (document.querySelector('#progress_bar').MaterialSlider){
         document.querySelector('#progress_bar').MaterialSlider.change(deplacement);
     }
-});
+});*/
