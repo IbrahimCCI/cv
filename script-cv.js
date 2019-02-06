ef89da2d6b6753cd300f27bfbfbe7684ab8fbbe7
IbrahimCCI <46899376+IbrahimCCI@users.noreply.github.com>
2019-02-06 19:41:40 +0100

    Update script-cv.js

--- script-cv.js
+++ script-cv.js
@@ -15,10 +15,4 @@ function myScroll() { // Définition de la fonction de scroll
     window.scroll(0, value) // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
 }
 
-/*window.addEventListener("scroll", function() {
-let deplacement  = (window.scrollY*100)/(document.body.offsetHeight-window.innerHeight);
 
-    if (document.querySelector('#progress_bar').MaterialSlider){
-        document.querySelector('#progress_bar').MaterialSlider.change(deplacement);
-    }
-});*/
